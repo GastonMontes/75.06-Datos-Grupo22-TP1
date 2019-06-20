6a044d4a72a792406cff5fd9a5dc09b7396c1f54
german-rotili <german.rotili@gmail.com>
2019-06-20 13:11:02 -0300

    TP2

---
+++ decompresor.py
@@ -0,0 +1,6 @@
+import ggzip
+
+ggzip.ggunzip('tp2/datos/auctions.csv.gzip', 'tp2/datos/auctions.csv')
+ggzip.ggunzip('tp2/datos/clicks.csv.gzip', 'tp2/datos/clicks.csv')
+ggzip.ggunzip('tp2/datos/events.csv.gzip', 'tp2/datos/events.csv')
+ggzip.ggunzip('tp2/datos/installs.csv.gzip', 'tp2/datos/installs.csv')
